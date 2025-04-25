2b1a0a8befbfea6e68b15faebc173addc39b1c46
E1ila <E1ila@users.noreply.github.com>
2025-04-25 08:25:49 +0300

    initial commit

---
+++ MeleeUtils.lua
@@ -0,0 +1,60 @@
+﻿local VERSION = "1.0.0"
+local VERSION_INT = 1.0000
+local ADDON_NAME = "MeleeUtils"
+local CREDITS = "by |cffb266ffKof|r @ |cffff2222Firemaw|r (era)"
+
+local L = MeleeUtils_BuildLocalization()
+local REALM = GetRealmName()
+
+local SharedMedia = LibStub:GetLibrary("LibSharedMedia-3.0")
+local DEFAULT_FONT_NAME = SharedMedia.MediaTable.font[SharedMedia.DefaultMedia.font]
+
+MeleeUtils = CreateFrame("Frame", "MeleeUtils")
+MeleeUtils:RegisterEvent("ADDON_LOADED")
+MeleeUtils.L = L
+MeleeUtils.Version = VERSION
+MeleeUtils.AddonName = ADDON_NAME
+
+MeleeUtilsGlobalVars = {
+    debug = false,
+    fontName = DEFAULT_FONT_NAME,
+    ver = VERSION_INT,
+}
+
+MeleeUtilsVars = {
+    enabled = true,
+}
+
+local function out(text, ...)
+    print(" |cffff8800{|cffffbb00MeleeUtils|cffff8800}|r", text, ...)
+end
+MeleeUtils.out = out
+
+local function debug(...)
+    if MeleeUtilsGlobalVars.debug then
+        out(...)
+    end
+end
+MeleeUtils.debug = debug
+
+function MeleeUtils:ADDON_LOADED(addonName)
+    if addonName ~= ADDON_NAME then
+        return
+    end
+    out("|cffffbb00v" .. tostring(VERSION) .. "|r " .. CREDITS .. ", " .. L["loaded-welcome"]);
+
+    -- Options UI
+    MeleeUtils.InterfacePanel:AddonLoaded()
+
+    MeleeUtils:SetScript("OnUpdate", function(self, ...)
+        MeleeUtils:OnUpdate(...)
+    end)
+end
+
+MeleeUtils:SetScript("OnEvent", function(self, event, a, b, ...)
+    MeleeUtils[event](self, a, b, ...)
+end)
+
+function MeleeUtils:OnUpdate()
+end
+
---
+++ Slash.lua
@@ -0,0 +1,24 @@
+local out = MeleeUtils.out
+
+SLASH_MELEEUTILS1 = "/meleeutils";
+SLASH_MELEEUTILS2 = "/mu";
+SLASH_MELEEUTILS3 = "/melee";
+SlashCmdList.MELEEUTILS = function(msg)
+    local _, _, cmd, arg1 = string.find(msg, "([%w]+)%s*(.*)$")
+    if not cmd then
+        out("Use MeleeUtils to track your grinding session yield. Options:")
+        out(" |cff00ff00/mu|r toggle logging on/off")
+    else
+        cmd = string.upper(cmd)
+        if "DEBUG" == cmd then
+            MeleeUtilsGlobalVars.debug = not MeleeUtilsGlobalVars.debug
+            if MeleeUtilsGlobalVars.debug then
+                out("Debug mode |cff00ff00enabled")
+            else
+                out("Debug mode |cffff0000disabled")
+            end
+        else
+            out("Unknown command " .. cmd)
+        end
+    end
+end

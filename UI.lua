f5912710dfe92b35a7dc2ec1faaf4cdfd6bc6114
E1ila <E1ila@users.noreply.github.com>
2025-04-27 08:13:17 +0300

    moved progressbar to ui

--- UI.lua
+++ UI.lua
@@ -2,6 +2,8 @@ local ADDON_NAME, addon = ...
 local MeleeUtils = addon.root
 local debug = MeleeUtils.Debug
 
+-- General -----------------------------------------------------------
+
 function MeleeUtils:InitUI()
     debug("Initializing UI")
     MeleeUtils_Parry_Texture:SetVertexColor(1, 0, 0)
@@ -45,6 +47,66 @@ end
 function MeleeUtils:DisableDarkBackdrop(frame)
     frame:SetBackdrop (nil)
 end
+
+
+-- Progress Bar -----------------------------------------------------------
+
+function MeleeUtils:CreateProgressBar(parent, list, index, height, padding, color, icon, gap)
+    local frame
+    pbId = pbId + 1
+    frame = CreateFrame("Frame", "MeleeUtils_PB"..tostring(pbId), parent, "MeleeUtils_StatusBar")
+    debug("Created progress bar", "name", frame:GetName(), "index", index)
+    if index > 0 then
+        local lastFrame = list[index].frame
+        frame:SetPoint("TOP", lastFrame, "BOTTOM", 0, -gap)
+    else
+        frame:SetPoint("TOP", parent, "TOP", 0, 0)
+    end
+    frame:SetPoint("LEFT", parent, "LEFT", 0, 0)
+    frame:SetPoint("RIGHT", parent, "RIGHT", 0, 0)
+    frame:SetHeight(height)
+    frame:SetBackdrop({
+        bgFile   = "Interface\\DialogFrame\\UI-DialogBox-Background",
+        edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+        edgeSize = 8, -- Border thickness
+        insets   = { left = padding, right = padding, top = padding, bottom = padding } -- Padding
+    })
+    frame:SetBackdropBorderColor(unpack(color)) -- Red border
+    frame:SetBackdropColor(0, 0, 0, 0.5) -- Black background with 50% opacity
+
+    local iconFrame = _G[frame:GetName().."_Icon"]
+    local barFrame  = _G[frame:GetName().."_Progress_Bar"]
+    local progFrame = _G[frame:GetName().."_Progress"]
+
+    progFrame:ClearAllPoints()
+    progFrame:SetPoint("TOPLEFT", iconFrame, "TOPRIGHT", 0, 0)
+    progFrame:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -padding, 2)
+
+    barFrame:SetStatusBarColor(unpack(color))
+
+    iconFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", padding, -padding)
+    iconFrame.icon:SetTexture(icon)
+    iconFrame:SetSize(height-padding*2, height-padding*2)
+    iconFrame.icon:SetSize(height-padding*2, height-padding*2)
+
+    return frame
+end
+
+function MeleeUtils:UpdateProgressBar(timer)
+    --debug("Updating progress for", timer.name, "expTime", timer.expTime, "duration", timer.duration)
+    if timer.duration > 0 and timer.expTime > GetTime() then
+        timer.frame:Show()
+        local progress = (timer.expTime - GetTime()) / timer.duration
+        _G[timer.frame:GetName().."_Progress_Bar"]:SetValue(progress)
+        return true
+    else
+        timer.frame:Hide()
+        return false
+    end
+end
+
+
+-------------------------------------------------------------
 
 function MeleeUtils:Rogue_SetCombo(n)
     if n < 5 then

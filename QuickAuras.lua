0b1c37b49c680165d62f1515ebf30e913e46ab09
E1ila <E1ila@users.noreply.github.com>
2025-06-03 22:22:42 +0200

    0.14.1

--- QuickAuras.lua
+++ QuickAuras.lua
@@ -9,7 +9,7 @@ local _ = LibStub("AceConsole-3.0")
 
 addon.root = AceAddon:NewAddon("QuickAuras", "AceConsole-3.0")
 local QA = addon.root
-QA.version = "0.14"
+QA.version = "0.14.1"
 QA.events = CreateFrame("Frame")
 QA.bags = {} -- items in bags
 QA.playerBuffs = {}

49122090b9aa43affc9af77fabb75f4b0b9db429
E1ila <E1ila@users.noreply.github.com>
2025-12-19 11:09:58 +0200

    printing stalagg and feugen works

--- BossLogic.lua
+++ BossLogic.lua
@@ -278,21 +278,18 @@ function Thaddius:CheckHP()
         if (minHP % 10 == 0 or (minHP <= 15 and minHP % 5 == 0)) and minHP ~= self.lastAnnouncedHP then
             self.lastAnnouncedHP = minHP
             local st = string.format(" >> %s %d%%  /  %s %d%%  << ", stalaggName, stalaggHP, feugenName, feugenHP)
-            out(st)
             SendChatMessage(st, "RAID")
         end
     elseif stalaggHP and stalaggHP > 0 then
         if (stalaggHP % 10 == 0 or (stalaggHP <= 15 and stalaggHP % 5 == 0)) and stalaggHP ~= self.lastAnnouncedHP then
             self.lastAnnouncedHP = stalaggHP
             local st = string.format(" >> %s %d%%  << ", stalaggName, stalaggHP)
-            out(st)
             SendChatMessage(st, "RAID")
         end
     elseif feugenHP and feugenHP > 0 then
         if (feugenHP % 10 == 0 or (feugenHP <= 15 and feugenHP % 5 == 0)) and feugenHP ~= self.lastAnnouncedHP then
             self.lastAnnouncedHP = feugenHP
             local st = string.format(" >> %s %d%%  << ", feugenName, feugenHP)
-            out(st)
             SendChatMessage(st, "RAID")
         end
     end
